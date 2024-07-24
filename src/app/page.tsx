32ac2993d9579821bc3e5e1d8672ca8b47c28ab6
DisonUyoga <disonobudho233@gmail.com>
2024-07-24 18:42:42 +0300

    clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,15 +25,14 @@ export default async function Home() {
   const user = await currentUser();
   const { data: session, error: sessionError } =
     await createClient().auth.getSession();
+  // check whether clerk user is logged in
   if (!user) {
-   
     return redirect("/sign-in");
   }
   if (!session.session) {
     return redirect("/sign-in");
   }
 
-  
   const { data, error } = await createClient()
     .from("products")
     .select("*, categories(*)")
